8f41c63d2c4211d1e9d0debc8aa9fae50c78d927
jon cannon <iamjoncannon@gmail.com>
2019-04-19 22:00:18 -0400

    committed

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const http = require('http');
 const https = require('https');
 const fs = require('fs')
 
-app.use(console.log('middleware is getting called'))
+app.use(()=>console.log('middleware is getting called'))
 
 // body parsing middleware
 app.use(express.json())
