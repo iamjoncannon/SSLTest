409e6c4b48cc30d4cd8a32795e390433d8e536c6
Ubuntu <ubuntu@ip-172-31-20-14.us-west-2.compute.internal>
2019-04-20 00:49:42 +0000

    main.js

--- main.js
+++ main.js
@@ -3,10 +3,20 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const { db  } = require('./server/db')
+// const { db  } = require('./server/db')
 const PORT = process.env.PORT || 3000
 const http = require('http');
 const https = require('https');
+const fs = require('fs')
+
+/*
+app.use((req,res)=>{
+
+console.log('hitting something',req.params, req.path, req.secure)
+res.status(200).send('goodbye world').end()
+
+})
+*/
 
 // body parsing middleware
 app.use(express.json())
@@ -16,17 +26,28 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, './public')))
 app.use(express.static(path.join(__dirname, './')))
 
-app.get('./server/.well-known/acme-challenge/:id', (req, res) =>{
 
-	res.send('hitting with ', req.params.id)
-	// res.sendFile(path.join(__dirname, './server/.well-known/acme-challenge/') + req.params.id)
-	res.end()
+app.get('/.well-known/acme-challenge/:id', (req, res) =>{
+
+        console.log('hitting with ', req.params)
+        let file = './.well-known/acme-challenge/' + req.params.id
+        // res.status(200).send('HITTING')
+        require('fs').readFile(file, (data) =>{
+
+        	console.log(data)
+	        res.status(200).send(data)
+	        res.end()
+        })
 })
 
-app.use('./server/api', require('./server/api')) // include our routes!
+
+// app.use('./server/api', require('./server/api')) // include our routes!
 
 app.get('*', (req, res) => {
+
+console.log('hitting *')
   res.sendFile(path.join(__dirname, './public/index.html'))
+  res.end()
 }) // Send index.html for any other requests
 
 // error handling middleware
@@ -37,36 +58,41 @@ app.use((err, req, res, next) => {
 
 // SSL certificates
 
-if(process.env.mode === 'secure'){
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/ssltest.joncannon.codes/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/ssltest.joncannon.codes/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/ssltest.joncannon.codes/chain.pem', 'utf8');
 
-	const privateKey = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/privkey.pem', 'utf8');
-	const certificate = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/cert.pem', 'utf8');
-	const ca = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/chain.pem', 'utf8');
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
 
-	const credentials = {
-		key: privateKey,
-		cert: certificate,
-		ca: ca
-	};
-
-	const httpsServer = https.createServer(credentials, app);
-}
+// const httpsServer = https.createServer(credentials, app);
 
 const httpServer = http.createServer(app);
 
+/* 
 async function startServer(){
 
 	// await db.sync()
 
     // console.log('db synced')
-    
-	httpServer.listen(PORT, () => {
+	console.log(process.env.mode)
 
-		console.log('HTTP Server running on port ', PORT);
-	});
 
-	process.env.mode === 'secure' ? httpsServer.listen(443, () => { console.log('HTTPS Server running on port 443'); }) : '' ;
+		httpsServer.listen(443, () => { console.log('HTTPS Server running on port 443', credentials); })
+
+
+//		httpServer.listen(80, () => {
+//			console.log('serving port 80')
+//	        });
+
 
 }
+*/
+//startServer()
 
-startServer()
+httpServer.listen(80, ()=>{console.log('started 80')});
+
+https.createServer(credentials, app).listen(443, (x)=>{console.log('started 443')} )
