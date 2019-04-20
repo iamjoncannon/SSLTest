d7553103ce9f473248991a53aedfa2413384f9b2
jon cannon <iamjoncannon@gmail.com>
2019-04-19 21:38:48 -0400

    res.end

--- main.js
+++ main.js
@@ -18,11 +18,14 @@ app.use(express.static(path.join(__dirname, './public')))
 app.use(express.static(path.join(__dirname, './')))
 
 app.use( (req, res, next) => {
+  
   if(!req.secure) {
 
     var secureUrl = "https://" + req.headers['host'] + req.url; 
     res.writeHead(301, { "Location":  secureUrl });
+    res.end()
   }
+
   next();
 });
 
